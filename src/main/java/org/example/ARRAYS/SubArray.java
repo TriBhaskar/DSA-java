5c1d615f4e13dd2f4302feec81cc8d01ac64c9b9
TriBhaskar <panthribhaskar@gmail.com>
2024-03-25 13:28:49 +0530

    DSA practice : Prefix sum array

--- src/main/java/org/example/ARRAYS/SubArray.java
+++ src/main/java/org/example/ARRAYS/SubArray.java
@@ -1,7 +1,26 @@
 package org.example.ARRAYS;
 
 public class SubArray {
-    public static void
+    public static void prefixSumArray(int arr[]){
+    int preSumArr[] = new int[arr.length];
+    int currentSum=0, maxSum=Integer.MIN_VALUE;
+    //caluclate prefixArray
+    preSumArr[0]= arr[0];
+        for (int i=1; i<preSumArr.length; i++){
+            preSumArr[i]=preSumArr[i-1] + arr[i];
+        }
+        for (int i=0; i<arr.length; i++){
+            //i = start
+            for (int j=i; j<arr.length; j++){
+                //j = end
+                currentSum = i==0 ? preSumArr[j] : preSumArr[j] - preSumArr[i-1];
+            }
+            if(maxSum < currentSum){
+                maxSum = currentSum;
+            }
+        }
+        System.out.println("max sum = "+maxSum);
+    }
     public static void maxSubarraySum(int arr[]){
         int currentSum=0, maxSum=Integer.MIN_VALUE;
         for (int i=0; i<arr.length; i++){
@@ -35,6 +54,7 @@ public class SubArray {
     public static void main(String[] args) {
         int arr[] = {1,-2,6,-1,3};
 //        printSubArray(arr);
-        maxSubarraySum(arr);
+//        maxSubarraySum(arr);
+        prefixSumArray(arr);
     }
 }
