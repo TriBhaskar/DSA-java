82060d30ee4af2db2a6e84f25adbf01c5c33e17d
TriBhaskar <panthribhaskar@gmail.com>
2024-03-29 23:28:00 +0530

    DSA practice : SortingAlgorithm - insertionSort improved

--- src/main/java/org/example/ARRAYS/SortingAlgorithm.java
+++ src/main/java/org/example/ARRAYS/SortingAlgorithm.java
@@ -2,17 +2,14 @@ package org.example.ARRAYS;
 
 public class SortingAlgorithm {
     public static void insertionSort(int arr[]){
-        int temp=0;
         for(int i=1; i<arr.length; i++){
-            int current = i;
+            int current = arr[i];
             int previous = i-1;
-            while(previous >=0 && arr[current]<arr[previous]){
-                temp = arr[current];
-                arr[current]= arr[previous];
-                arr[previous]=temp;
+            while(previous >=0 && arr[previous]>current){
+                arr[previous+1]=arr[previous];
                 previous--;
-                current--;
             }
+            arr[previous+1] = current;
         }
     }
     public static void selectionSort(int arr[]){
@@ -46,7 +43,7 @@ public class SortingAlgorithm {
         }
     }
     public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2,9,7,0};
+        int arr[] = {3,4,2,9,7,0};
 //        bubbleSort(arr);
 //        selectionSort(arr);
         insertionSort(arr);
