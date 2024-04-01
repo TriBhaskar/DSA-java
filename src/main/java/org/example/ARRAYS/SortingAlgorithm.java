0a146895830b5771d3e496667bcd5137155baaf5
TriBhaskar <panthribhaskar@gmail.com>
2024-04-01 22:17:59 +0530

    DSA practice : SortingAlgorithm -  countingSort

--- src/main/java/org/example/ARRAYS/SortingAlgorithm.java
+++ src/main/java/org/example/ARRAYS/SortingAlgorithm.java
@@ -1,6 +1,27 @@
 package org.example.ARRAYS;
 
 public class SortingAlgorithm {
+    public static void countingSort(int arr[]){//he time complexity of the countingSort function is O(n + k),
+        int largest = Integer.MIN_VALUE;
+        for (int i=0; i<arr.length; i++){
+            largest = Math.max(largest,arr[i]);
+        }
+        int[] count = new int[largest+1];
+
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+        //sorting
+        int j=0;
+        for(int i=0; i<count.length; i++){
+            while(count[i]>0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+    }
     public static void insertionSort(int arr[]){
         for(int i=1; i<arr.length; i++){
             int current = arr[i];
